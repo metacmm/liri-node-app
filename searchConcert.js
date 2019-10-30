ac0f6a75e297df5d3059f771b13e1a1d2a622a5a
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2019-10-30 15:29:11 -0700

    add searchConcert and searchmovie api.

--- searchConcert.js
+++ searchConcert.js
@@ -1,3 +1,22 @@
-var searchConcert = function(name){
+var axios = require("axios");
+var moment = require("moment");
 
+var searchConcert = function(name){
+    let app_id = "codingbootcamp";
+    let artist = name.split(" ").join("+");
+    axios.get("https://rest.bandsintown.com/artists/"+ artist + "/events?app_id=" + app_id)
+    .then(function(response){
+        console.log(response);
+        response.data.forEach(function(concert){
+            console.log("+-------------------------------------------------");
+            console.log("Name of the venue: " + concert.venue.name);
+            console.log("Venue location: " + concert.venue.city + " " + concert.venue.country);
+            console.log("Date of the Event: " + moment(concert.datetime).format("MM/DD/YYYY"));
+        })
+    })
+    .catch(function(error){
+        console.log("error is" + error);
+    });
 }
+
+searchConcert("billie eilish");
---
+++ searchMovie.js
@@ -0,0 +1,22 @@
+var axios = require("axios");
+
+var searchMovie = function(name){
+    let api_key = "trilogy";
+    axios.get("http://www.omdbapi.com/?apikey=" + api_key + "&t=" + name)
+    .then(function(response){
+        console.log(response);
+        console.log("Title of the movie: " + response.data.Title);
+        console.log("Year the movie came out: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
+        console.log("Country where the movie was produced: " + response.data.Country);
+        console.log("Language of the movie: " + response.data.Language);
+        console.log("Plot of the movie: " + response.data.Plot);
+        console.log("Actors in the movie: " + response.data.Actors);
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+};
+
+searchMovie("blade runner");
